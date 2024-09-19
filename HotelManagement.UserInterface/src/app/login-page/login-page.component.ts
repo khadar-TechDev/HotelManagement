bdce84bd66c8ad5308abd058e24697168661799d
Khadar khan <khadar.khan@zelis.com>
2024-09-19 01:58:17 -0700

    Added some design for login page

--- HotelManagement.UserInterface/src/app/login-page/login-page.component.ts
+++ HotelManagement.UserInterface/src/app/login-page/login-page.component.ts
@@ -7,6 +7,10 @@ import { Component } from '@angular/core';
   templateUrl: './login-page.component.html',
   styleUrl: './login-page.component.css'
 })
-export class LoginPageComponent {
+export class LoginPageComponent
+{
+userIDPlaceHolder:string = "Enter your user ID"
+passwordPlaceHolder:string = "Enter your password"
+
 
 }
