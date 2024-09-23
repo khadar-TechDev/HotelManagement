9cb40eacf1b10722fb1d6372117e0ed835f88c3a
Khadar khan <khadar.khan@zelis.com>
2024-09-23 05:20:31 -0700

    Added signup page component and did design for signup component

--- HotelManagement.UserInterface/src/app/app.component.ts
+++ HotelManagement.UserInterface/src/app/app.component.ts
@@ -1,13 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { FooterComponent } from './footer/footer.component';
+import { AppRoutingModule } from './app.routes';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [
     RouterOutlet,
-    FooterComponent
+    FooterComponent,
+    AppRoutingModule
   ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
